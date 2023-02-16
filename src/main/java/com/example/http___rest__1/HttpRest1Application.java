a90e24f6ff59eab2ab250f8019c3ce57f65d1671
Антон <antonolegovich2017@icloud.com>
2023-02-16 17:10:13 +0600

    dz

---
+++ src/main/java/com/example/http___rest__1/HttpRest1Application.java
@@ -0,0 +1,13 @@
+package com.example.http___rest__1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpRest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpRest1Application.class, args);
+    }
+
+}
